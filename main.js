ad49c5e8ce7d50b84af4d474faa214e5c72a0fb1
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-08-19 14:29:30 +0300

    Init voices

--- main.js
+++ main.js
@@ -17,3 +17,32 @@ var isFirefox = typeof InstallTrigger !== 'undefined';
 
 // Chrome 1+
 var isChrome = !!window.chrome && !!window.chrome.webstore;
+
+// Init voices array
+let voices = [];
+
+const getVoices = () => {
+    voices = synth.getVoices();
+
+    //Loop through voices and create an option for each one
+    voices.forEach(voice => {
+        // Create option element
+        const option = document.createElement("option");
+        // Fill option with voice and language
+        option.textContent = voice.name + "(" + voice.lang + ")";
+
+        //Set needed option attributes
+        option.setAttribute("data-lang", voice.lang);
+        option.setAttribute("data-name", voice.name);
+    })
+}
+
+//Run code depending on the browser
+if (isFirefox) {
+    getVoices();
+}
+if (isChrome) {
+    if (synth.onvoiceschanged !== undefined) {
+        synth.onvoiceschanged = getVoices;
+    }
+}
