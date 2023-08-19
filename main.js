4c8f596a830571e468e8220b64c8c46016fe1be4
Volodymyr Burkovskyi <v.burkovskyi@gamzix.com>
2023-08-19 14:08:45 +0300

    Initialized elements DOM in js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Init SpeechSynth API
+const synth = window.speechSynthesis;
+
+// DOM Elements
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
